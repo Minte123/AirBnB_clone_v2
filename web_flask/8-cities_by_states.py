37bd07afeb7d2c32469782a1437db0718aad6ea4
Minte123 <netacademy1986minte@yahoo.com>
2022-10-28 20:37:06 +0300

    Web framework

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
 """
-Flask App
-"""
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from os import getenv
-
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_conn(self):
-    """ close connection """
-    storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
-def get_state():
-    """ get all states_list """
-    state = storage.all('State').values()
-    return render_template("7-states_list.html", state=state)
-
-
-@app.route("/cities_by_state", strict_slashes=False)
-def get_cities_by_state():
-    """ get all states_list """
-    states = storage.all("State").values()
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
